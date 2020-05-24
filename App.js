17fefb12e368545629175c83317d9f451fee6a91
saket sinha <saketsinha23@gmail.com>
2020-05-25 04:12:15 +0530

    done with splash

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Container from './components';
 
+import SplashScreen from 'react-native-splash-screen';
+
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return <Container />;
 };
 
